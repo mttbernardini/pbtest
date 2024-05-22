99a21fed09508d74abd033a52ce4d2a91aa0e016
Matteo Bernardini <matteo@teobe.net>
2024-05-22 10:55:01 +0800

    use multiple linters

--- src/pbtest.py
+++ src/pbtest.py
@@ -1,4 +1,9 @@
 import pedalboard
 
+# the following line fails with: Name "pedalboard.Plugin" is not defined [name-defined]
+gain: pedalboard.Plugin
+
 # the following line fails with: Module has no attribute "Gain" [attr-defined]
 gain = pedalboard.Gain()
+
+reveal_type(gain)
